638e09df3e86497f141d85eefaf07118c19a20b9
Nour Salama <n.salama.ict@hotmail.com>
2022-11-01 16:34:10 +0100

    Some Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+    imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h1>Hello world</h1>',
+})
+export class EventsAppComponent {
+  title = 'angular-fundamentals';
+}
