92860f267ff42c8be231a237d82dfb6b93f07b73
Nour Salama <n.salama.ict@hotmail.com>
2022-11-02 10:43:57 +0100

    static imgaes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Hello world</h1>',
+  template: `
+    <h1>Hello world</h1>
+    <img src="/assets/images/basic-shield.png" />
+  `,
 })
 export class EventsAppComponent {
   title = 'angular-fundamentals';
