c2ae5dc9ca6bd34e08bd412d7899e6d4fc9434f9
Nour Salama <n.salama.ict@hotmail.com>
2022-11-02 16:08:30 +0100

    Passing from Child to Parent data TS

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'event-thumbnail',
@@ -13,11 +13,16 @@ import { Component, Input } from "@angular/core";
       <span>&nbsp;</span>
       <span>{{event.location.city}}, {{event.location.country}}</span>
     </div>
-  </div>
+    <button class="btn btn-primary p-2 mt-2" (click)="handleClickMe()">Click me!</button>
+  </div> 
   `
 })
 
 export class EventsThumbnailComponent {
-  @Input()
-  event: any;
+  @Input() event:any
+  @Output() eventClick = new EventEmitter();
+
+  handleClickMe() {
+    console.log(this.event.name);
+  }
 }
