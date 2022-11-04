cb600ce1b696a4239836fd29cd6a7d0f037017ed
Nour Salama <n.salama.ict@hotmail.com>
2022-11-04 13:12:45 +0100

    ngSwitch & ngIf

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -7,16 +7,24 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div class="bg-light bg-gradient-light text-secondary p-4">
       <h2>{{event.name}}</h2>
       <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
+      <div [ngSwitch]="event?.time">Time: {{event.time}}
+        <span *ngSwitch="'8:00 am'">(Early start)</span>
+        <span *ngSwitch="'10:00 am'">(Late start)</span>
+        <span *ngSwitchDefault>(Normal start)</span>
+      </div>
       <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        <span>Online URL: {{event?.onlineUrl}}</span>
       </div>
     </div>
   </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     `]
 })
