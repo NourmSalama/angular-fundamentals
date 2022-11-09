fd47e43933101d5e2de029a7806f529ade0b2eec
Nour Salama <n.salama.ict@hotmail.com>
2022-11-09 13:05:44 +0100

    Routing

--- src/app/common/toastr.service.ts
+++ src/app/common/toastr.service.ts
@@ -7,12 +7,15 @@ export class ToastrService {
   success(message : string, title?: string) {
     toastr.success(message, title);
   }
+
   info(message : string, title?: string) {
     toastr.info(message, title);
   }
+
   warning(message : string, title?: string) {
     toastr.warning(message, title);
   }
+
   error(message : string, title?: string) {
     toastr.error(message, title);
   }
